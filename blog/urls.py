f38e38bd0bff74b99383e1c47b2f42b5e5a94bae
taiChantarungsee <tai.jantarungsee@gmail.com>
2017-08-17 23:37:57 +0100

    Got rid of unecessary urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,7 @@ from django.views.generic import TemplateView
 from blog.views import *
 
 urlpatterns = [ # using the template {{ url 'url_name'}} we can refer to the URL in the template without hard coding it, e.g. for a link. 
-    url(r'^$', TemplateView.as_view(template_name='blog/main.html')),
-    url(r'^list/$', post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
-    url(r'^post/(?P<pk>\d+)/edit/$', post_edit, name='post_edit'),
-    url(r'^post/new/$', post_new, name='post_new'),
+    #url(r'^$', TemplateView.as_view(template_name='blog/main.html')),
 
     #URLs for character builder
     #url(r'^dnd/$', char_builder, name='char_builder'),
@@ -18,11 +14,4 @@ urlpatterns = [ # using the template {{ url 'url_name'}} we can refer to the URL
     url(r'^logout/$', auth_views.logout, {'template_name': 'blog/logout.html'}, name='logout'),
     url(r'^character/detail/(?P<pk>\d+)/$', character_detail, name='character_detail'),    
 
-
-    #URLs for polling
-    # ex: /polls/
-    url(r'^$/polls', IndexView.as_view(), name='index'), # Combine this (and relevant views with 'post_list'?)
-    url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', vote, name='vote'),
 ]
